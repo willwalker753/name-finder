023cf8c70150565d2a4f0432c992cfdd4786094c
Will Walker <williamwalker753@gmail.com>
2021-03-30 02:24:09 -0500

    fix last name not showing

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const submitHandler = () => {
         findName();
     });
 }
-const findName = e => {
-    
+const findName = e => {  
     let nameStr = $('#input-name').val();
     let letter = $('#input-letter').val().toLowerCase();
     let nameArr = [];
@@ -17,19 +16,19 @@ const findName = e => {
         if(nameStr.charAt(i) !== ' ') {
             tempStr = tempStr.concat(nameStr.charAt(i));
         }
-        else {
+        if(nameStr.charAt(i) === ' ' || i === nameStr.length-1){
             nameArr.push(tempStr.toLowerCase());
             tempStr = '';
         }
     }
+    console.log(nameArr)
     let result = '';
     for(let i=0; i<nameArr.length; i++) {
-        if(nameArr[i].charAt(0) == letter && nameArr[i].charAt(nameArr[i].length-1) == letter) {
+        if(nameArr[i].charAt(0) === letter && nameArr[i].charAt(nameArr[i].length-1) === letter) {
             result = result.concat(`${nameArr[i]}, `);
         }
     }
     result = result.slice(0, result.length-2);
-    console.log(result);
     $('#results').empty().append(`<p>${result}</p>`);
 }
 
